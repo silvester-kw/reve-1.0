90c0b708d2b0d6b34fea65c951193c02f660665b
Asweh Debrej <18221145@std.stei.itb.ac.id>
2024-05-30 00:58:08 +0700

    firebase test

--- app/_layout.js
+++ app/_layout.js
@@ -1,20 +1,77 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "react-native-reanimated";
 
-import { useColorScheme } from "@/hooks/useColorScheme";
+// import auth from "@react-native-firebase/auth";
 
+import { initializeApp, getApp, getApps } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  initializeAuth,
+  getReactNativePersistence,
+} from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+
+import { useColorScheme } from "@/hooks/useColorScheme";
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
 export default function RootLayout() {
+  // const [initializing, setInitializing] = useState(true);
+  // const [user, setUser] = useState(null);
+
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
+
+  //   const app = initializeApp(firebaseConfig);
+
+  let app;
+  let auth;
+  if (!getApps().length) {
+    try {
+      app = initializeApp(firebaseConfig);
+      auth = initializeAuth(app, {
+        persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  } else {
+    app = getApp();
+    auth = getAuth(app);
+  }
+
+  // const onAuthStateChanged = (user) => {
+  //   setUser(user);
+  //   if (initializing) {
+  //     setInitializing(false);
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   const subscriber = 1
+  //   return subscriber;
+  // }, []);
 
   useEffect(() => {
     if (loaded) {
@@ -25,6 +82,10 @@ export default function RootLayout() {
   if (!loaded) {
     return null;
   }
+
+  // if (initializing) {
+  //   return null;
+  // }
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
