77ed72cd2391b2415ec3f7445e11e30c33ac300b
Silvester Kresna W. P. P. <18221049@std.stei.itb.ac.id>
2024-05-31 01:14:46 +0700

    tambah data

---
+++ data/closet.js
@@ -0,0 +1,58 @@
+export const Closet = [
+  {
+    id: "1",
+    name: "Blue Jeans",
+    brand: "Brand B",
+    price: 30000,
+    size: "L",
+    category: "pants",
+    rentStart: "2 Apr 2024",
+    rentEnd: "7 Apr 2024",
+    status: "Shipment",
+    image: require("@/assets/images/shirts/shirt1.png"),
+    description:
+      "CONSCIOUS CHOICE Shirt in soft cotton flannel with a turn-down collar. buttons down the front and a yoke with a box pleat at the back. Relaxed fit with an open chest pocket, long sleeves with buttoned cuff* and a gently rounded hem.",
+  },
+  {
+    id: "2",
+    name: "White T-Shirt",
+    brand: "Brand C",
+    price: 30000,
+    size: "L",
+    rentStart: "2 Apr 2024",
+    rentEnd: "7 Apr 2024",
+    status: "Return Now!",
+    category: "shirt",
+    image: require("@/assets/images/shirts/shirt2.png"),
+    description:
+      "CONSCIOUS CHOICE Shirt in soft cotton flannel with a turn-down collar. buttons down the front and a yoke with a box pleat at the back. Relaxed fit with an open chest pocket, long sleeves with buttoned cuff* and a gently rounded hem.",
+  },
+  {
+    id: "3",
+    name: "Dummy 3",
+    brand: "Brand C",
+    price: 30000,
+    size: "L",
+    rentStart: "2 Apr 2024",
+    rentEnd: "7 Apr 2024",
+    status: "Return Now!",
+    category: "shirt",
+    image: require("@/assets/images/shirts/shirt3.png"),
+    description:
+      "CONSCIOUS CHOICE Shirt in soft cotton flannel with a turn-down collar. buttons down the front and a yoke with a box pleat at the back. Relaxed fit with an open chest pocket, long sleeves with buttoned cuff* and a gently rounded hem.",
+  },
+  {
+    id: "4",
+    name: "Dummy 4",
+    brand: "Brand C",
+    price: 30000,
+    size: "L",
+    rentStart: "2 Apr 2024",
+    rentEnd: "7 Apr 2024",
+    status: "Return Now!",
+    category: "shirt",
+    image: require("@/assets/images/shirts/shirt4.png"),
+    description:
+      "CONSCIOUS CHOICE Shirt in soft cotton flannel with a turn-down collar. buttons down the front and a yoke with a box pleat at the back. Relaxed fit with an open chest pocket, long sleeves with buttoned cuff* and a gently rounded hem.",
+  },
+];
